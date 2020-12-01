277a5313f9cdb7908bddddfdf7c8bcac9373ea69
dev <dev@don.red>
2020-12-02 01:20:40 +0800

    fix(login): fix login redirect

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,13 @@
-export const ACCESS_TOKEN_KEY = 'weibo_app_access_token';
-export const UID_KEY = 'weibo_app_uid';
+export const ACCESS_TOKEN_KEY = '';
+export const UID_KEY = '';
 export const getUid = () => localStorage.getItem(UID_KEY);
 
 export const APP_KEY = '3696852244';
 export const APP_SECRET = '52eaa55c068e880f58b81cab494f819e';
-export const REDIRECT_URI = encodeURIComponent(`${window.location.origin}/login`);
+export const REDIRECT_URI = encodeURIComponent(`${window.location.origin}${window.location.pathname}login`);
 
 export const LOGIN_URL = `https://mock.don.red/weibo/oauth2/authorize?client_id=${APP_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email`;
 
 export const getAccessCode = code => `https://mock.don.red/weibo/oauth2/access_token?client_id=${APP_KEY}&client_secret=${APP_SECRET}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&code=${code}`;
-
 
 export const COMMENT_PAGESIZE = 5;
