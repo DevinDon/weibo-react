1c6ac9e2792acfd19b6dde43453b6a4d2edde220
dev <dev@don.red>
2020-12-01 21:10:09 +0800

    fix(router): fix suspense style

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,27 @@
-import { Route, Switch } from 'react-router-dom';
+import { Spin } from 'antd';
 import React, { lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import styles from './index.module.scss';
 
 const Home = lazy(() => import('../pages/Home'));
 const Login = lazy(() => import('../pages/Login'));
 const New = lazy(() => import('../pages/New'));
+const Demo = lazy(() => import('../pages/Demo'));
 
 const Router = () => (
   <Suspense
-    fallback="Loading..."
+    fallback={
+      <div className={styles.spin}>
+        <Spin size="large" />
+      </div>
+    }
   >
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/new" component={New} />
       <Route exact path="/comments/:id" component={New} />
+      <Route exact path="/demo" component={Demo} />
     </Switch>
   </Suspense>
 );
