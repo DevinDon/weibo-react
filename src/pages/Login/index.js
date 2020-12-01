b9484ebefe94d0596e77f377e07ac4d9d222ccf4
dev <dev@don.red>
2020-12-02 00:50:59 +0800

    perf(login): optimize login loading page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,20 @@
 import { getAccess } from 'actions/account';
+import { Spin } from 'antd';
 import queryString from 'query-string';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'redux-react-hook';
+import styles from '../../router/index.module.scss';
 
 const Login = () => {
   const dispatch = useDispatch();
   const { query: { code } } = queryString.parseUrl(window.location.href);
   useEffect(() => {
-    if (code) {
-      dispatch(getAccess({ code }));
-    }
+    code && dispatch(getAccess({ code }));
   }, [code, dispatch]);
-  return <div>{code}</div>;
+  return <div className={styles.spin}>
+    <Spin size="large" />
+    <p>登录中，请稍候……</p>
+  </div>;
 };
 
 export default Login;
