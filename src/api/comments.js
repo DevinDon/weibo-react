deec7ef03ba356cce7ae1074a3bbd659a70a479f
Devin Don <i.inf@outlook.com>
2021-01-31 09:03:21 +0800

    perf(constants): add api constant

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,20 +1,21 @@
+import { API } from '../constants';
 import ajax from '../utils/ajax';
 
 export function createComment(params) {
-  return ajax.post('https://demo.don.red/weibo/api/2/comments/create.json', {
+  return ajax.post(`${API}/2/comments/create.json`, {
     data: params,
     headers: { 'content-type': 'application/x-www-form-urlencoded' }
   });
 }
 
 export function getComments(params) {
-  return ajax.get('https://demo.don.red/weibo/api/2/comments/show.json', {
+  return ajax.get(`${API}/2/comments/show.json`, {
     params,
   });
 }
 
 export function deleteComment(params) {
-  return ajax.post('https://demo.don.red/weibo/api/2/comments/destroy.json', {
+  return ajax.post(`${API}/2/comments/destroy.json`, {
     data: params,
     headers: { 'content-type': 'application/x-www-form-urlencoded' }
   });
--- src/api/timeline.js
+++ src/api/timeline.js
@@ -1,13 +1,14 @@
+import { API } from '../constants';
 import ajax from '../utils/ajax';
 
 export function getHomeTimeline(params) {
-  return ajax.get('https://demo.don.red/weibo/api/2/statuses/home_timeline.json', { params });
+  return ajax.get(`${API}/2/statuses/home_timeline.json`, { params });
 }
 
 export function getPublicTimeline(params) {
-  return ajax.get('https://demo.don.red/weibo/api/2/statuses/public_timeline.json', { params });
+  return ajax.get(`${API}/2/statuses/public_timeline.json`, { params });
 }
 
 export function getPost(params) {
-  return ajax.get('https://demo.don.red/weibo/api/2/statuses/show.json', { params });
+  return ajax.get(`${API}/2/statuses/show.json`, { params });
 }
